16c278a7cfcd23949db047737fce9c31c17c069b
Kendrik <kendrikhernandez@icloud.com>
2025-03-14 11:52:04 -0400

    Modified server.js to add new routes.

--- backend/server/server.js
+++ backend/server/server.js
@@ -8,6 +8,10 @@ const getUserByIdRoute = require('./routes/userGetUserById')
 const dbConnection = require('./config/db.config')
 const editUser = require('./routes/userEditUser')
 const deleteUser = require('./routes/userDeleteAll')
+const getFavoriteByIdRoute = require('./routes/favoriteReadFavorite')
+const editFavorite = require('./routes/favoriteEditFavorite')
+const deleteAllFavorite = require('./routes/favoriteDeleteAll')
+const createNewFavorite = require('./routes/favoriteCreateNew')
 
 require('dotenv').config();
 const SERVER_PORT = 8081
@@ -21,6 +25,10 @@ app.use('/user', getAllUsersRoute)
 app.use('/user', getUserByIdRoute)
 app.use('/user', editUser)
 app.use('/user', deleteUser)
+app.use('/favorite', getFavoriteByIdRoute)
+app.use('/favorite', editFavorite)
+app.use('/favorite', deleteAllFavorite)
+app.use('/favorite', createNewFavorite)
 
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`The backend service is running on port ${SERVER_PORT} and waiting for requests.`);
