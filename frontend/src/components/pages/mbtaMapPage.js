7964f3f5519232fe65c5a5bce26ac518eb598884
Aluminite <10791181+Aluminite@users.noreply.github.com>
2025-04-06 00:21:43 -0400

    Minor refinements

--- frontend/src/components/pages/mbtaMapPage.js
+++ frontend/src/components/pages/mbtaMapPage.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {MapContainer, TileLayer, CircleMarker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import {decode} from "@googlemaps/polyline-codec";
 import axios from "axios";
 import Form from 'react-bootstrap/Form';
 
@@ -13,7 +12,7 @@ const MbtaMap = () => {
     useEffect(() => {
         async function fetchData() {
             const result = await axios(
-                'https://api-v3.mbta.com/routes?filter%5Btype%5D=0%2C1%2C2%2C3',
+                'https://api-v3.mbta.com/routes'
             );
             return result.data;
         }
@@ -32,7 +31,7 @@ const MbtaMap = () => {
     }, []);
 
     function handleRouteChange({currentTarget: dropdown}) {
-        selectedRoute.current = dropdown.value;
+        selectedRoute.current = transitRoutes.find((route) => route.id === dropdown.value);
         updateRouteVehicles(selectedRoute.current);
     }
 
@@ -40,7 +39,7 @@ const MbtaMap = () => {
         if (route != null) {
             async function fetchData() {
                 const result = await axios(
-                    'https://api-v3.mbta.com/vehicles?filter%5Broute%5D=' + route,
+                    'https://api-v3.mbta.com/vehicles?filter%5Broute%5D=' + route.id
                 );
                 return result.data;
             }
@@ -48,21 +47,39 @@ const MbtaMap = () => {
             fetchData().then((response) => {
                 setRouteVehicles(response.data);
             });
+        } else {
+            setRouteVehicles([]);
         }
     }
-
-    /*const enc_polyline = "euqaGrkvpLGFG@O@MBIDgAhAqBpBuAnA]^kAlBUb@i@z@_BbCSXEJEHm@bAGLGHqAtBk@|@oEdHCBABEF}@rA]j@]f@y@jAIJMNGFm@t@sVn[{@dA]`@u@n@YV]T{@f@w@XuAb@}@NYFg@F]H{@B[?s@?cBOiBAiAAeBAq@A??{@?{@A_@?{PM_EJq@?}@CuA?oFCw@Am@Ck@Go@Im@Mu@Qq@So@Uq@]u@c@w@i@]Yi@c@]_@cAoAc@o@u@kA_C_Es@qAiB{CkBmCcAqAmFuGsCoDqCiD{AkBqA}A}AkB}@kAmAkBa@w@c@aA]{@Yy@YaAU{@c@mBO{@_@iCU_CEu@AQASEw@Am@AS?UAu@AoBB_BLgDJqAJaARwAVaB^cB`@_BlAmEf@gBd@eBd@aBhD}LXaAp@qC`@}BRqANoA~@}H`AeIp@}F\\oCXkCR{Bb@uC^_DZoC`AeIn@oFZwCDWHo@BQL}@NsA??h@uELiABSBQbAiIV{BDe@D_@JgAHyAF_EAaBE_BOgCSwB_@oC_@qB]sA[iAo@oBu@mBmCkHkA{CoE}LgB{EsBmFq@iBaDgIAGCE}AmEoBsF}@cCqAiD{@wBaE}KCKCIKWkBcFkB{Ew@cBS[GMKOa@o@g@s@k@q@wBwBiA}@[USQ{BiB}BcByAaAkDaCe@[??g@_@uGqEiD}B}DqCwE_DmFuDqLaIcFoDMKAAMI{ByAyLsI_PsKcJiGwHkFoGmEaMoImXiRoXgRwPiLu@e@qFuDcGeEiCgBaC}AkJuGkA{@mB_BmAgAgAeAIK??cBgByAaBaAkAaC}CyBcDw@iAqBiDy@yA{@aBw@{Aw@iBcAeCy@qBs@eB{A{DsAwDmBgFKSIUGMaAyCaHiRaDaJmAgD_@}@mAcDgBaFw@cCi@cB}@oCs@qBKUM[mAgD[{@??kBcFoAgDa@gAyBeGcAuCy@{B_DaIo@iBGOGOgDgJaBsEOa@iBcFOa@]_A{@{ByCkIWq@kAeDgAwCuAyDwGwQmAmDa@eAg@{A}@yBw@gB_BaD??MY}@}AKS_AaByAcCIK}@sAuAkBW_@YYY_@y@}@[[e@g@WW_CyBgA}@iCmBq@a@g@]c@Yo@c@KEKGQI[O]Qc@Sm@YSKw@]}@[oBq@y@WaBa@gBa@qBc@}A[yA]u@QeA[qAc@oAc@cBq@qB_Ag@YoCaB_C}Ao@g@]Y[W}@w@u@q@wHcHyAsAcGqFc@a@k@i@m@g@_BqA}AgAm@a@{A}@mAq@_CkAuDyBqF{CeE{B_YqOIEGESKuGoDcDkBcE{BwGmDk@QsB{@y@Yi@Mm@Mk@Go@MeA?{@CI?sA@mDWiQkAmFe@cAIq@GkE]]?qTLaJDmA@S?M?G?cAI}@O????gAYo@Ue@SgBiAqCgB_CwAyBuAwAaAe@WwH}EgAq@sEyCiC}AqCsAYMIEGCk@Sa@O{Ae@oCs@{A[}A]iBg@sAYkEeAuG_B_LmCoG}AoD_AICKCaDq@yD_A{Bi@aBYeDg@??s@MaBM{AMaIWmB@qF@e@?kD@yACyAUoBe@_BgAkAwAWm@_BmBwAmBoBsCoBaDcB{C{AwCy@gBoB{EsAuDeAkDcAqD}@kDs@{CmB_JoBwJI_@??y@{Dw@mDqE_TkAeG[eBo@wEy@gIOmBScE_@iM_@iPS}GCoA?kCBoBByAHiBFy@^_EL_A\\mB\\gB`@wAl@qBrAaEvCkIt@qBj@}ArCuHvBmFxC{HtAgDt@uBb@wA\\uATgA\\gBR{ARwANmBHqADkBBoCCoBA{@MeCM{BOgBs@{Kc@kG??QeCAOM}Aw@}LYkEmAkO[mDeBeQw@sHIkAQsAUyB??c@iEwAsMi@yFm@sFSkBe@cE[mD{@qIg@_FKu@yCaZUuBYaCYsCYgCM{AUyCIuCO_GOuE]kIGyAKmAUiCMeAa@}Ba@qBc@_B[q@IYCKAIAOQm@GMo@qAaAeBo@cASY}@gAkDyCmCeC{@aA}@mAmAsBGK[m@_@w@k@{Ae@yAo@wB_@{AsA_FaAuDaAyDIWy@wC}BuIaA_EEM??Og@}@yDUoA_@{Bc@yDYoCKaBMw@aA}J_@yDMqAOeAUcBg@kCOq@c@}A[cAc@kAmAyCo@sAw@oAa@q@eB{CeByCkCsEmBkDgCmEiCyEi@aAy@iBq@mBUs@Qk@g@sBc@_C_@aC]oCmAqJc@yC_@wBe@uBm@sB{@sB}@qB_AkBcAkBkFmKyA}Cm@iAGK_AgBmFuKiDaHeBgDkAuByBkEeAwBaAkBo@qAm@mAeAmBsBcD_B{By@eAsBeC}BqCiE_Fot@my@qA{Aa@e@eAuAa@m@}@}Aq@{AUq@uAmDo@yAq@oA]m@m@}@q@aAu@{@q@u@m@k@oAcAi@_@_Ao@sDmCs@q@a@a@WWg@m@mB_C??_HmI{@gAk@w@g@u@_@q@c@}@c@cAe@mAWy@K[Qo@U{@[cBMm@Ks@K_AK_Ae@iGa@{FAQcBcTU_Ck@uEgBuLc@_D_@iDS}BGwAEaCGeHWu`@?gAMkPCeD?y@@cA@aAFy@H{@VsB@K@GJ}@VcBjBcMh@eE??DWZuBJ{@Dy@B}@?o@Aq@KwAKg@EOQm@Qg@g@cAs@}@[_@GCc@YYOc@MeAKa@?qFD_PRwHDG?E?a@@i@?O?S?S?o@G_@G[Iy@YYM[Sw@i@s@q@q@{@U]O[Q]eD_IcFcMiLeYs@oBi@iBgAmE_AsD]iA_@kAg@iAq@qAaAcB_AuAoCgEy@{Au@yA_@}@aDoHmHoQyCcH_CuFoCuGoAsBsAiB_B{AkBoAcB{@_L_FaDsAsBm@cC_@qCScCOoCU_BW}Ac@cAe@OIcAm@kAy@gAgAsA{AoA{AUYeAkAiHoI]a@cCoCoAuAuAwAuEaF";
-    const dec_polyline = decode(enc_polyline);*/
 
     return (
         <div>
             <Form.Select onChange={handleRouteChange}>
-                <option>Choose route</option>
-                {transitRoutes.map(route => (
-                    <option key={route.id} value={route.id}>
-                        {route.attributes["description"]}: {route.attributes["long_name"]}
-                    </option>
-                ))}
+                <option value={null}>Choose route</option>
+                {transitRoutes.map(route => {
+                    let name = "";
+                    switch (route.attributes["type"]) {
+                        case 0:
+                        case 1:
+                            name = "Subway: " + route.attributes["long_name"];
+                            break;
+                        case 2:
+                            name = "Commuter Rail: " + route.attributes["long_name"];
+                            break;
+                        case 3:
+                            name = "Bus: " + route.attributes["short_name"] + " - " + route.attributes["long_name"];
+                            break;
+                        case 4:
+                            name = "Ferry: " + route.attributes["long_name"];
+                            break;
+                        default:
+                    }
+                    return (
+                        <option key={route.id} value={route.id}>
+                            {name}
+                        </option>
+                    );
+                })}
             </Form.Select>
             <div className="map">
                 <MapContainer center={[42.359149, -71.0581643]} zoom={10} scrollWheelZoom={true}>
@@ -75,6 +92,7 @@ const MbtaMap = () => {
                     {routeVehicles.map(vehicle => (
                         <CircleMarker key={vehicle.id}
                                       center={[vehicle.attributes.latitude, vehicle.attributes.longitude]}
+                                      pathOptions={{color: "#" + selectedRoute.current.attributes["color"]}}
                                       radius={10}></CircleMarker>
                     ))}
                 </MapContainer>
